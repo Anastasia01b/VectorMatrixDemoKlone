f45c47b513ba97025f0c22d18f49ed15eb6b3319
Anastasia01b <101938365+Anastasia01b@users.noreply.github.com>
2023-10-06 11:23:10 +0300

    Update Vector.cs

--- VectorDemo/Vector.cs
+++ VectorDemo/Vector.cs
@@ -1,232 +1,129 @@
 using System;
-using VectorDemo;
 
-namespace MatrixDemo
+namespace VectorDemo
 {
-    public class Matrix
+    public class Vector
     {
-        private double[,] data;
-        public Matrix()
-        {
-            data = new double[3, 3];
-       
-        }
+        private double[] data;
 
-        public Matrix(int r, int c)
+        public Vector()
         {
-            data = new double[r, c];
-        }
-
-        public Matrix(double[,] data)
-        {
-           this.data = data;
+            data = new double[3];
         }
         
-        public double[,] GetArrayRef
+        public Vector(int N)
         {
-            get
-            {
-               return data;
-            }
-        }
-
-        public int Row
-        {
-            get
-            {
-               return data.GetLength(0);
-            }
-        }
-
-        public int Col
-        {
-            get
-            {
-                return data.GetLength(1);
-            }
-        }
-
-        private static void ValidateMatrixSizes(Matrix m1, Matrix m2)
-        {
-            if (m1.Row != m2.Row || m1.Col != m2.Col)
-            {
-                throw new InvalidOperationException("Matrix sizes do not match");
-            }
-        }
-
-        private static void ValidateMatrixAndVectorSizes(Matrix matrix, Vector vector)
-        {
-            if (matrix.Row != vector.Length || matrix.Col != vector.Length)
-            {
-                throw new InvalidOperationException("Matrix row or column size is not equal to the length of the vector");
-            }
-        }
-
-        public static Matrix operator*(Matrix m1, double c)
-        {
-           int row =m1.Row;
-           int col =m1.Col;
-           Matrix result = new Matrix(row, col);
-
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    result.data[i, j] = m1.data[i, j] * c;
-                }
-            }
-
-            return result;
-
+            data = new double[N];
         }
         
-        public static Matrix operator*(double c, Matrix m1)
+        public Vector(double[] d)
         {
-            return m1 * c;
+           data = d ;
+        }
+        private void ValidateSize(Vector o)
+        {
+            if (data.Length != o.Length)
+            {
+                throw new InvalidOperationException("The operation cannot be performed for vectors of different sizes");
+            }
+        }
+
+        public Vector Add(Vector o)
+        {
+            ValidateSize(o);
+
+            Vector result = new Vector(data.Length);
+           for (int i =0; i< data.Length; i++)
+            {
+                result[i] = data[i] + o[i];
+            }
+           return result;
         }
         
-        public static Matrix operator*(Matrix m1, Matrix m2)
+        public Vector Sub(Vector o)
         {
-            int row1 = m1.Row;
-            int col1 = m1.Col;
-            int col2 = m2.Col;
-            int row2 = m2.Row;
+            ValidateSize(o);
 
-            if (col1 != row2)
+            Vector result = new Vector(data.Length);
+            for (int i=0; i<data.Length; i++)
             {
-                throw new InvalidOperationException("The number of colums is not equal tothe number of rows");
-            }
-
-            Matrix result = new Matrix(row1, col2);
-
-            for (int i = 0; i < row1; i++)
-            {
-                for (int j = 0; j < col2; j++)
-                {
-                    double sum = 0;
-                    for (int k = 0; k < col1; k++)
-                    {
-                        sum += m1.data[i, k] * m2.data[k, j];
-                    }
-                    result.data[i, j] = sum;
-                }
-            }
-
-            return result;
-        }
-        
-        public static Matrix operator+(Matrix m1, Matrix m2)
-        {
-            ValidateMatrixSizes(m1, m2);
-
-            int row1 = m1.Row;
-            int col1 = m1.Col;
-            int col2 = m2.Col;
-            int row2 = m2.Row;
-
-            Matrix result = new Matrix(row1, col1);
-            for (int i =0; i<row1; i++)
-            {
-                for (int j =0; j<col1; j++)
-                {
-                    result.data[i, j] = m1.data[i, j] + m2.data[i, j];
-                }
+                result[i]= data[i] - o[i];
             }
             return result;
         }
         
-        public static Matrix operator-(Matrix m1, Matrix m2)
+        public double Scalar(Vector o)
         {
-            ValidateMatrixSizes(m1, m2);
+            ValidateSize(o);
 
-            int row1 = m1.Row;
-            int col1 = m1.Col;
-            int col2 = m2.Col;
-            int row2 = m2.Row;
-
-            Matrix result = new Matrix(row1, col1);
-            for (int i = 0; i < row1; i++)
+            double result = 0;
+            for (int i =0; i< data.Length;i++)
             {
-                for (int j = 0; j<col1; j++)
-                {
-                    result.data[i, j]= m1.data[i, j]- m2.data[i, j];
-                }
+                result += data[i] * o[i];
+            }
+            return result;
+
+        }
+        
+        public static Vector operator*(Vector v, double c)
+        {
+            Vector result = new Vector(v.Length);
+            for (int i=0; i< v.Length; i++)
+            {
+                result[i] = v[i] * c;
             }
             return result;
         }
         
-        public static Vector operator*(Matrix m1, Vector v1)
+        public static Vector operator*(double c,Vector v)
         {
-            ValidateMatrixAndVectorSizes(m1 , v1);
-            int row = m1.Row;
-            int col = m1.Col;
-
-            Vector result = new Vector(row);
-            for (int i = 0; i < row;i++)
-            {
-                double sum = 0;
-                for (int j=0; j<col; j++)
-                {
-                    sum += m1.data[i, j] * v1[j];
-                }
-                result[i] = sum;
-            }
-            return result;
-        }
-        
-        public static Vector operator*(Vector v1,Matrix m1)
-        {
-            ValidateMatrixAndVectorSizes(m1, v1);
-
-            int row = m1.Row;
-            int col = m1.Col;
-
-            Vector result = new Vector(col);
-            for (int j = 0; j < col; j++)
-            {
-                double sum = 0;
-                for (int i = 0; i < row; i++)
-                {
-                    sum += v1[i]* m1.data[i, j];
-                }
-                result[j] = sum;
-            }
-            return result;
-        }
-        public override bool Equals(object obj)
-        {
-            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
-            {
-                return false;
-            }
-
-            Matrix m = (Matrix) obj;
-            if (Row != m.Row || Col != m.Col)
-                return false;
-            for(int i=0;i<Row;++i)
-                for(int j=0;j<Col;++j)
-                    if (data[i, j] != m.data[i, j])
-                        return false;
-            return true;
+            return v*c;
         }
 
         public override string ToString()
         {
-            var row = Row;
-            var col = Col;
-            var lines = new string[row];
+            string result = "(" + string.Join(";", data) + ")";
 
-            for (int i = 0; i < row; i++)
+            return result;
+        }
+
+        public int Length
+        {
+            get
             {
-                var line = new double[col];
-                for (int j = 0; j < col; j++)
-                {
-                    line[j] = data[i, j];
-                }
-                lines[i] = string.Join(" ", line);
+               return data.Length;
+            }
+        }
+        
+        public double[] GetArrayRef
+        {
+            get
+            {
+                return data;
+            }
+        }
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= data.Length)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
             }
 
-            return string.Join(Environment.NewLine, lines);
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return data[index];  
+            }
+
+            set
+            {
+                ValidateIndex(index);
+                data[index] = value;
+            }
         }
     }
 }
